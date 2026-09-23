ef1026b86fd3159e7015ced415a1a9c63a9bc7d4
陳音宇 <t115360223@ntut.org.tw>
2026-09-23 16:24:52 +0800

    Add files via upload

---
+++ project2_26_/Project2_26/Project2_26/source/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    puts(
+        "  CCCCC  \n"
+        " C     C \n"
+        "C       C\n"
+        "C       C\n"
+        " C     C \n"
+        "\n"
+        "        Y\n"
+        "       Y \n"
+        "YYYYYYY  \n"
+        "       Y \n"
+        "        Y\n"
+        "\n"
+        "        Y\n"
+        "       Y \n"
+        "YYYYYYY  \n"
+        "       Y \n"
+        "        Y"
+    );
+
+    return 0;
+}
